b348711b21ccb5fdd372a38d33eee05de755d69f
Steve Sohcot <ssohcot@gmail.com>
2019-11-28 22:43:23 -0500

    additional login file for chp 23

---
+++ chp23/login/index.php
@@ -0,0 +1,36 @@
+<?php include_once("../includes/a_first.php");?>
+
+<?php define("PAGE_TITLE", "Log In");?>
+
+<?php Template_Header();?>
+
+<div class="container">
+
+	<h1>Log In</h1>
+	
+	<form action="../controllers/controller_login.php" method="POST">
+	    <div class="input-group input-group-lg form-group">
+	        <label for="email" class="sr-only">Email</label>
+	        <input type="text" name="email" id="email" class="form-control input-lg" placeholder="Email" data-required="true" />
+	    </div>
+	          
+	    <div class="input-group input-group-lg form-group">
+	        <label for="password" class="sr-only">Password</label>
+	        <input type="password" name="password" id="password" class="form-control input-lg" placeholder="Password" data-required="true" />
+	    </div>
+
+	    <div class="form-group">
+	    	<input type="hidden" name="login" value="1" />
+	    	<input type="submit" class="btn btn-primary btn-lg" value="Log In" />
+	    	<a href="../signup" class="btn  btn-lg">Sign Up</a>
+	    </div> 
+
+	</form>
+
+</div> <!-- /.container -->
+
+<?php Template_Footer();?>
+
+<?php include_once("../includes/a_last.php");?>
+
+
